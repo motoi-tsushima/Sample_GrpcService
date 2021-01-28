3d9b6fb0602144c4790973ac6d817dca1eb39b2a
motoi-tsushima <motoi.tsushima@gmail.com>
2021-01-28 17:31:04 +0900

    tag 1.1.0.0 MyFunction と Calc を追加した。

--- Sample_GrpcService/Services/GreeterService.cs
+++ Sample_GrpcService/Services/GreeterService.cs
@@ -22,5 +22,28 @@ namespace Sample_GrpcService
                 Message = "Hello " + request.Name
             });
         }
+
+        public override Task<MyFunctionRply> MyFunction(MyRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new MyFunctionRply
+            {
+                Message = "Reply ! " + request.Parameter1 + "=" + request.ParameterIntValue
+            });
+        }
+
+        public override Task<CalcResult> Calc(CalcParameter parameter, ServerCallContext context)
+        {
+            Int32 value1 = parameter.Parameter1;
+            Int32 value2 = parameter.Parameter2;
+
+            return Task.FromResult(new CalcResult
+            {
+                Addition = value1 + value2,
+                Subtraction = value1 - value2,
+                Multiplication = value1 * value2,
+                Division = value1 / value2
+            });
+        }
+
     }
 }
